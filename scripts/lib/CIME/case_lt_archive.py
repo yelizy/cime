15c9debefe00cd266521bba09a0686d9d3420b20
Alice Bertini <aliceb@ucar.edu>
2017-03-16 15:56:10 -0600

    update to copy_dirs_local and start at copy_dir_ssh

--- scripts/lib/CIME/case_lt_archive.py
+++ scripts/lib/CIME/case_lt_archive.py
@@ -2,7 +2,7 @@
 long term archiving
 """
 
-import shutil, glob, re, os
+import shutil, glob, re, os, errno
 
 from CIME.XML.standard_module_setup import *
 from CIME.utils                     import expect, does_file_have_string, append_status, run_cmd
@@ -66,20 +66,125 @@ def _copy_dirs_hsi(dout_s_root, dout_l_msroot, dout_l_hpss_accnt, dryrun,
 
 
 ###############################################################################
-def _copy_dirs_ssh(dout_s_root, dout_l_msroot, dout_l_ssh_loc, dryrun,
-                   dout_l_delete):
+# check_ssh_key
+###############################################################################
+def check_ssh_key(dout_l_ssh_login, dout_l_ssh_mach):
+
+    ssh_key = True
+    # check if ssh key is set for passwordless access to the remote machine
+    try:
+        output = subprocess.check_output( "ssh -oNumberOfPasswordPrompts=0 {0}@{1} 'echo hello'".format(dout_l_ssh_login,dout_l_ssh_mach), 
+                                 stderr=subprocess.STDOUT,
+                                 shell=True)
+    except subprocess.CalledProcessError as e:
+        ssh_key = False
+        msg = 'ERROR: unable to connect to remote host {0}@{1}'.format(dout_l_ssh_login,dout_l_ssh_mach)
+        msg += '\n sshkeys to the remote login should be setup prior to running lt_archive in mode' \
+                    'DOUT_L_MODE=copy_dirs_ssh.'
+
+    return ssh_key, msg
+
+###############################################################################
+def _copy_dirs_ssh(dout_s_root, dout_l_msroot, dout_l_ssh_mach, dryrun,
+                   dout_l_delete, dout_l_ssh_login):
 ###############################################################################
 
     logger.debug('In copy_dirs_ssh...')
     success = True
     msg = ""
     
+    # check sshkeys
+    ssh_key, msg = check_ssh_key(dout_l_ssh_login, dout_l_ssh_mach):
+    if not ssh_key:
+        return ssh_key, msg
+
+    src = dout_s_root
+    dst = dout_l_ssh_login+'@'+dout_l_ssh_mach+':'+dout_l_msroot
+    errors = []
+
+## START HERE replacing the commands
+
+    # check if dst exists; recursively create if exists
+    if not os.path.exists(dst):
+        if dryrun:
+            logger.info('dryrun: makedirs %s' %dst)
+        else:
+            os.makedirs(dst)
+
+    names = os.listdir(src)
+    for name in names:
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+
+        try:
+            if symlinks and os.path.islink(srcname):
+                linkto = os.readlink(srcname)
+                if dryrun:
+                    logger.info('dryrun: create symlink %s -> %s' %linkto %dstname)
+                else:
+                    os.symlink(linkto, dstname)
+            elif os.path.isdir(srcname):
+                if dryrun:
+                    logger.info('dryrun: calling copytree for %s to %s' %srcname %dstname)
+                    if dout_l_delete:
+                        logger.info('dryrun: delete dir tree %s' %srcname)
+                else:
+
+                    try:
+                        shutil.copytree(srcname, dstname, symlinks, ignore=None)
+                        # delete the local directory
+                        if dout_l_delete:
+
+                            try:
+                                os.rmtree(scrname)
+                            except OSError as why:
+                                errors.extend((srcname, str(why)))
+
+                    except (IOError, os.error) as why:
+                        errors.append((srcname, dstname, str(why)))
+            else:
+                if dryrun:
+                    logger.info('dryrun: calling copy2 for %s to %s' %srcname %dstname)
+                    if dout_l_delete:
+                        logger.info('dryrun: delete local file %s' %srcname)
+                else:
+
+                    try:
+                        shutil.copy2(srcname, dstname)
+                        # delete the local file
+                        if dout_l_delete:
+                            try:
+                                os.remove(srcname)
+                            except OSError as why:
+                                errors.extend((srcname, str(why)))
+                    except OSError as why:
+                        errors.extend((srcname, dstname, str(why)))
+
+        except (IOError, os.error) as why:
+            errors.append((srcname, dstname, str(why)))
+        except Error as err:
+            errors.extend(err.args[0])
+
+    try:
+        if dryrun:
+            logger.info('dryrun: calling copystat for %s to %s' %src %dst)
+        else:
+            shutil.copystat(src, dst)
+    except WindowsError:
+        # can't copy file access times on Windows
+        pass
+    except OSError as why:
+        errors.extend((src, dst, str(why)))
+
+    if errors:
+        msg = Error(errors)
+        success = False
+
     return success, msg
 
 
 ###############################################################################
-def _copy_dirs_local(dout_s_root, dout_l_msroot, dout_l_arc_root, dryrun,
-                     dout_l_delete):
+def _copy_dirs_local(dout_s_root, dout_l_msroot, dryrun, dout_l_delete):
 ###############################################################################
 
     logger.debug('In copy_dirs_local...')
@@ -101,6 +206,7 @@ def _copy_dirs_local(dout_s_root, dout_l_msroot, dout_l_arc_root, dryrun,
     for name in names:
         srcname = os.path.join(src, name)
         dstname = os.path.join(dst, name)
+
         try:
             if symlinks and os.path.islink(srcname):
                 linkto = os.readlink(srcname)
@@ -111,24 +217,50 @@ def _copy_dirs_local(dout_s_root, dout_l_msroot, dout_l_arc_root, dryrun,
             elif os.path.isdir(srcname):
                 if dryrun:
                     logger.info('dryrun: calling copytree for %s to %s' %srcname %dstname)
+                    if dout_l_delete:
+                        logger.info('dryrun: delete dir tree %s' %srcname)
                 else:
-                    copytree(srcname, dstname, symlinks, ignore=None)
+
+                    try:
+                        shutil.copytree(srcname, dstname, symlinks, ignore=None)
+                        # delete the local directory
+                        if dout_l_delete:
+
+                            try:
+                                os.rmtree(scrname)
+                            except OSError as why:
+                                errors.extend((srcname, str(why)))
+
+                    except (IOError, os.error) as why:
+                        errors.append((srcname, dstname, str(why)))
             else:
                 if dryrun:
                     logger.info('dryrun: calling copy2 for %s to %s' %srcname %dstname)
+                    if dout_l_delete:
+                        logger.info('dryrun: delete local file %s' %srcname)
                 else:
-                    copy2(srcname, dstname)
+
+                    try:
+                        shutil.copy2(srcname, dstname)
+                        # delete the local file
+                        if dout_l_delete:
+                            try:
+                                os.remove(srcname)
+                            except OSError as why:
+                                errors.extend((srcname, str(why)))
+                    except OSError as why:
+                        errors.extend((srcname, dstname, str(why)))
+
         except (IOError, os.error) as why:
             errors.append((srcname, dstname, str(why)))
-            # catch the Error from the recursive copytree so that we can
-            # continue with other files
         except Error as err:
             errors.extend(err.args[0])
+
     try:
         if dryrun:
             logger.info('dryrun: calling copystat for %s to %s' %src %dst)
         else:
-            copystat(src, dst)
+            shutil.copystat(src, dst)
     except WindowsError:
         # can't copy file access times on Windows
         pass
@@ -168,9 +300,8 @@ def case_lt_archive(case, dryrun, force):
         dout_l_msroot = case.get_value("DOUT_L_MSROOT")
         dout_l_hpss_accnt = case.get_value("DOUT_L_HPSS_ACCNT")
         dout_l_mode = case.get_value("DOUT_L_MODE")
-        dout_l_delete = case.get_value("DOUT_L_DELETE_LOC_FILES")
-        dout_l_ssh_loc = case.get_value("DOUT_L_SSH_LOC")
-        dout_l_arc_root = case.get_value("DOUT_L_ARC_ROOT")
+        dout_l_delete = case.get_value("DOUT_L_DELETE_LOCAL_FILES")
+        dout_l_ssh_mach = case.get_value("DOUT_L_SSH_MACHINE")
         lid = time.strftime("%y%m%d-%H%M%S")
 
         msg = ""
@@ -184,11 +315,10 @@ def case_lt_archive(case, dryrun, force):
            (success, msg) = _copy_dirs_hsi(dout_s_root, dout_l_msroot, dout_l_hpss_accnt, 
                                            dryrun, dout_l_delete)
         elif dout_l_mode == "copy_dirs_ssh":
-           (success, msg) = _copy_dirs_ssh(dout_s_root, dout_l_msroot, dout_l_ssh_loc, 
+           (success, msg) = _copy_dirs_ssh(dout_s_root, dout_l_msroot, dout_l_ssh_mach,
                                            dryrun, dout_l_delete)
         elif dout_l_mode == "copy_dirs_local":
-           (success, msg) = _copy_dirs_local(dout_s_root, dout_l_msroot, dout_l_arc_loc, 
-                                             dryrun, dout_l_delete)
+           (success, msg) = _copy_dirs_local(dout_s_root, dout_l_msroot, dryrun, dout_l_delete)
         else:
             expect(False,
                    "lt_archive: unrecognized DOUT_L_MODE '"+dout_l_mode+"'."
